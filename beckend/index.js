6a8c5b98c0fb34628a1dffaf936e69427d9b55da
Bruno <bruno_151997@hotmail.com>
2020-03-23 12:13:10 -0300

    Backend primeiro dia

---
+++ beckend/index.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+
+const app = express();
+
+app.get('/', (request, response)=>{
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Bruno Rodrigues'
+    });
+});
+app.listen(3333);
